875b21a7f93f7df0dc7d4c58375437032357e2bc
hydra <abcxyzkhong@gmail.com>
2023-11-19 19:35:34 +0700

    Initial commit

--- nvim/lua/plugins/dev/cmp-config.lua
+++ nvim/lua/plugins/dev/cmp-config.lua
@@ -1,140 +1,141 @@
 -- nvim-cmp setup
-local cmp = require 'cmp'
-local luasnip = require 'luasnip'
-local copilot = require 'copilot_cmp'
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+local copilot = require("copilot_cmp")
 local source_mapping = {
-  nvim_lsp = "[]",
-  luasnip = "[]",
-  buffer = "[]",
-  treesitter = "[]",
-  path = "[]",
-  rg = "[]",
-  copilot = "[]"
+	nvim_lsp = "[]",
+	luasnip = "[]",
+	buffer = "[]",
+	treesitter = "[]",
+	path = "[]",
+	rg = "[]",
+	copilot = "[]",
 }
 local duplicates = {
-  buffer = 1,
-  path = 1,
-  nvim_lsp = 0,
-  luasnip = 1,
+	buffer = 1,
+	path = 1,
+	nvim_lsp = 0,
+	luasnip = 1,
 }
-luasnip.config.setup {}
-copilot.setup {}
-cmp.setup {
-snippet = {
-  expand = function(args)
-    luasnip.lsp_expand(args.body)
-  end,
-},
-mapping = cmp.mapping.preset.insert {
-  ['<C-j>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-k>'] = cmp.mapping.scroll_docs(4),
-    ['<C-b>'] = cmp.mapping.complete {},
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-  },
+luasnip.config.setup({})
+copilot.setup({})
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-j>"] = cmp.mapping.scroll_docs(-4),
+		["<C-k>"] = cmp.mapping.scroll_docs(4),
+		["<C-b>"] = cmp.mapping.complete({}),
+		["<CR>"] = cmp.mapping.confirm({
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		}),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+	}),
 
-  sources = {
-    { name = "copilot", group_index = 1 },
-    { name = "luasnip", group_index = 2 },
-    { name = "nvim_lsp",group_index = 2 },
-    { name = "buffer", group_index = 3 },
-    { name = "html-css",
-      option = {
-        max_count = {}, -- not ready yet
+	sources = {
+		{ name = "copilot", group_index = 1 },
+		{ name = "luasnip", group_index = 2 },
+		{ name = "nvim_lsp", group_index = 2 },
+		{ name = "buffer", group_index = 3 },
+		{
+			name = "html-css",
+			option = {
+				max_count = {}, -- not ready yet
 
-        enable_on = {
-            "html",
-            "js",
-            "jsx",
-            "vue"
-        }, -- set the file types you want the plugin to work on
-        file_extensions = { "css", "sass", "less" }, -- set the local filetypes from which you want to derive classes
-        style_sheets = {
-          -- example of remote styles, only css no js for now
-          "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
-          -- "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css",
-        }
-      }
-    },
-    { name = "nvim_lua" },
-    { name = "path", group_index = 3 },
-  },
+				enable_on = {
+					"html",
+					"js",
+					"jsx",
+					"vue",
+				}, -- set the file types you want the plugin to work on
+				file_extensions = { "css", "sass", "less" }, -- set the local filetypes from which you want to derive classes
+				style_sheets = {
+					-- example of remote styles, only css no js for now
+					"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
+					-- "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css",
+				},
+			},
+		},
+		{ name = "nvim_lua" },
+		{ name = "path", group_index = 3 },
+	},
 
-  -- window = {
-  --   completion = cmp.config.window.bordered{
-  --     border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-  --     side_padding = -999,
-  --     col_offset = 0
-  --   },
-  --   documentation = {
-  --     border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-  --     winhighlight = "NormalFloat:NormalFloat,FloatBorder:TelescopeBorder",
-  --   }
-  -- },
-  formatting = {
-    fields = {"kind", "abbr", "menu"},
-    format = function (entry, item)
-      local kind = require("lspkind").cmp_format { mode = "symbol_text", maxwidth = 40 }(entry,item)
-      local source = entry.source.name
-      local menu = source_mapping[source]
-      local duplicates_default = 0
-      local strings = vim.split(kind.kind, "%s", {trimempty = true })
-      kind.kind = strings[1] .. " "
-      kind.menu = menu
+	-- window = {
+	--   completion = cmp.config.window.bordered{
+	--     border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+	--     side_padding = -999,
+	--     col_offset = 0
+	--   },
+	--   documentation = {
+	--     border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+	--     winhighlight = "NormalFloat:NormalFloat,FloatBorder:TelescopeBorder",
+	--   }
+	-- },
+	formatting = {
+		fields = { "kind", "abbr", "menu" },
+		format = function(entry, item)
+			local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 40 })(entry, item)
+			local source = entry.source.name
+			local menu = source_mapping[source]
+			local duplicates_default = 0
+			local strings = vim.split(kind.kind, "%s", { trimempty = true })
+			kind.kind = strings[1] .. " "
+			kind.menu = menu
 
-      local max_width = 80
-      if max_width ~= 0 and #kind.abbr > max_width then
-        kind.abbr = string.sub(kind.abbr, 1, max_width -1) .. "..."
-      end
-      kind.dup = duplicates[source] or duplicates_default
-      return kind
-    end
-  }
-}
+			local max_width = 80
+			if max_width ~= 0 and #kind.abbr > max_width then
+				kind.abbr = string.sub(kind.abbr, 1, max_width - 1) .. "..."
+			end
+			kind.dup = duplicates[source] or duplicates_default
+			return kind
+		end,
+	},
+})
 --
 -- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-  sources = cmp.config.sources({
-    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-  }, {
-    { name = 'buffer' },
-  })
+cmp.setup.filetype("gitcommit", {
+	sources = cmp.config.sources({
+		{ name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
+	}, {
+		{ name = "buffer" },
+	}),
 })
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' }
-  }
+cmp.setup.cmdline({ "/", "?" }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = "buffer" },
+	},
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
+cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{ name = "cmdline" },
+	}),
 })
--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,165 +1,158 @@
 return {
-  "aca/emmet-ls",
-  
-  "derektata/lorem.nvim",
-  "tikhomirov/vim-glsl",
-  {
-    "nvim-pack/nvim-spectre",
-    opts = {}
-  },
-  -- Indent-blankline
-  { 
-    "lukas-reineke/indent-blankline.nvim",
-    event = "BufReadPost",
-    main = "ibl",
-    opts = {
-      
-    } 
-  },
+	"aca/emmet-ls",
 
-  -- statusline/winbar component
-  {
-    "SmiteshP/nvim-navic",
-    requires = "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      require("nvim-navic").setup()
-    end
-  },
-  {
-    "utilyre/barbecue.nvim",
-    name = "barbecue",
-    version = "*",
-    lazy = false,
-    dependencies = {
-      "SmiteshP/nvim-navic",
-      "nvim-tree/nvim-web-devicons", -- optional dependency
-    },
-    opts = {
-      symbols = {
-      },
-      kinds = {
-      },
-    }
-  },
-  { 
-    'numToStr/Comment.nvim',
-    opts = {} 
-  },
-  {
-    "folke/trouble.nvim",
-    cmd = "TroubleToggle",
-  },
-  { 'folke/which-key.nvim', opts = {} },
+	"derektata/lorem.nvim",
+	"tikhomirov/vim-glsl",
+	{
+		"nvim-pack/nvim-spectre",
+		opts = {},
+	},
+	-- Indent-blankline
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		event = "BufReadPost",
+		main = "ibl",
+		opts = {},
+	},
 
-  { "kdheepak/lazygit.nvim" },
-  { "windwp/nvim-autopairs" },
-  { 
-    "simrat39/symbols-outline.nvim",
-    opts = {}
-  },
-  { "rmagatti/goto-preview" },
-  
-  {
-    "folke/todo-comments.nvim",
-    event = "BufRead",
-    config = function()
-      require("todo-comments").setup()
-    end,
-  },
-  {
-    "SmiteshP/nvim-navbuddy",
-    requires = {
-      "neovim/nvim-lspconfig",
-      "SmiteshP/nvim-navic",
-    },
-  },
-  {
-    "tpope/vim-fugitive",
-    cmd = {
-      "G",
-      "Git",
-      -- "Gdiffsplit",
-      "Gvdiffsplit",
-      "Gread",
-      "Gwrite",
-      "Ggrep",
-      "GMove",
-      "GDelete",
-      "GBrowse",
-      "GRemove",
-      "GRename",
-      "Glgrep",
-      "Gedit"
-    },
-    ft = { "fugitive" }
-  },
-  {
-    -- Adds git releated signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      -- See `:help gitsigns.txt`
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-        untracked    = { text = '┆' },
-      },
-    },
-  },
+	-- statusline/winbar component
+	{
+		"SmiteshP/nvim-navic",
+		requires = "neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			require("nvim-navic").setup()
+		end,
+	},
+	{
+		"utilyre/barbecue.nvim",
+		name = "barbecue",
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"SmiteshP/nvim-navic",
+			"nvim-tree/nvim-web-devicons", -- optional dependency
+		},
+		opts = {
+			symbols = {},
+			kinds = {},
+		},
+	},
+	{
+		"numToStr/Comment.nvim",
+		opts = {},
+	},
+	{
+		"folke/trouble.nvim",
+		cmd = "TroubleToggle",
+	},
+	{ "folke/which-key.nvim", opts = {} },
 
-  {
-    'akinsho/toggleterm.nvim',
-    version = "*",
-    opts = {
-      open_mapping = [[<c-\>]],
-      shade_filetypes = {},
-      autochdir = false, -- when neovim changes it current directory the terminal will change it's own when next it's opened
-      highlights = {
-        -- highlights which map to a highlight group name and a table of it's values
-        -- NOTE: this is only a subset of values, any group placed here will be set for the terminal window split
-        NormalFloat = {
-          link = 'Normal'
-        },
-      },
-      shade_terminals = true, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
-      start_in_insert = true,
-      insert_mappings = true, -- whether or not the open mapping applies in insert mode
-      terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
-      persist_size = true,
-      persist_mode = true, -- if set to true (default) the previous terminal mode will be remembered
-      direction = 'float',
-      close_on_exit = true, -- close the terminal window when the process exits
+	{ "kdheepak/lazygit.nvim" },
+	{ "windwp/nvim-autopairs" },
+	{
+		"simrat39/symbols-outline.nvim",
+		opts = {},
+	},
+	{ "rmagatti/goto-preview" },
 
+	{
+		"folke/todo-comments.nvim",
+		event = "BufRead",
+		config = function()
+			require("todo-comments").setup()
+		end,
+	},
+	{
+		"SmiteshP/nvim-navbuddy",
+		requires = {
+			"neovim/nvim-lspconfig",
+			"SmiteshP/nvim-navic",
+		},
+	},
+	{
+		"tpope/vim-fugitive",
+		cmd = {
+			"G",
+			"Git",
+			-- "Gdiffsplit",
+			"Gvdiffsplit",
+			"Gread",
+			"Gwrite",
+			"Ggrep",
+			"GMove",
+			"GDelete",
+			"GBrowse",
+			"GRemove",
+			"GRename",
+			"Glgrep",
+			"Gedit",
+		},
+		ft = { "fugitive" },
+	},
+	{
+		-- Adds git releated signs to the gutter, as well as utilities for managing changes
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			-- See `:help gitsigns.txt`
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
+			},
+		},
+	},
 
-      -- shell = vim.o.shell, -- change the default shell
-      shell = require("until").shell(), --set shell   pwsh
-      
-      
-      auto_scroll = true, -- automatically scroll to the bottom on terminal output
-      -- This field is only relevant if direction is set to 'float'
-      float_opts = {
-        -- The border key is *almost* the same as 'nvim_open_win'
-        -- see :h nvim_open_win for details on borders however
-        -- the 'curved' border is a custom border type
-        -- not natively supported but implemented in this plugin.
-        border = 'curved' ,
-        -- like `size`, width and height can be a number or function which is passed the current terminal
-        -- winblend = 3,
-      },
-    }
-  },
-  { 'nvim-telescope/telescope.nvim', version = '*', dependencies = { 'nvim-lua/plenary.nvim' } },
-  {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    -- NOTE: If you are having trouble with this installation,
-    --       refer to the README for telescope-fzf-native for more instructions.
-    build = 'make',
-    cond = function()
-      return vim.fn.executable 'make' == 1
-    end,
-  },
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		opts = {
+			open_mapping = [[<c-\>]],
+			shade_filetypes = {},
+			autochdir = false, -- when neovim changes it current directory the terminal will change it's own when next it's opened
+			highlights = {
+				-- highlights which map to a highlight group name and a table of it's values
+				-- NOTE: this is only a subset of values, any group placed here will be set for the terminal window split
+				NormalFloat = {
+					link = "Normal",
+				},
+			},
+			shade_terminals = true, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
+			start_in_insert = true,
+			insert_mappings = true, -- whether or not the open mapping applies in insert mode
+			terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
+			persist_size = true,
+			persist_mode = true, -- if set to true (default) the previous terminal mode will be remembered
+			direction = "float",
+			close_on_exit = true, -- close the terminal window when the process exits
 
+			-- shell = vim.o.shell, -- change the default shell
+			shell = require("until").shell(), --set shell   pwsh
+
+			auto_scroll = true, -- automatically scroll to the bottom on terminal output
+			-- This field is only relevant if direction is set to 'float'
+			float_opts = {
+				-- The border key is *almost* the same as 'nvim_open_win'
+				-- see :h nvim_open_win for details on borders however
+				-- the 'curved' border is a custom border type
+				-- not natively supported but implemented in this plugin.
+				border = "curved",
+				-- like `size`, width and height can be a number or function which is passed the current terminal
+				-- winblend = 3,
+			},
+		},
+	},
+	{ "nvim-telescope/telescope.nvim", version = "*", dependencies = { "nvim-lua/plenary.nvim" } },
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		-- NOTE: If you are having trouble with this installation,
+		--       refer to the README for telescope-fzf-native for more instructions.
+		build = "make",
+		cond = function()
+			return vim.fn.executable("make") == 1
+		end,
+	},
 }
