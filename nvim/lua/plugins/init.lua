c939a960098b0e6358be92a027a0c075ebf8a35e
hydra07 <abcxyzkhong@gmail.com>
2023-12-17 18:01:28 +0700

    update nvim

--- nvim/lua/plugins/init.lua
+++
@@ -1,7 +0,0 @@
--- lualine
-require("plugins.dev.lualine-config")
-require("plugins.dev.cmp-config")
-require("plugins.dev.dev")
-require("plugins.dev.telescope-config")
-require("plugins.dev.suggest-code")
-require("plugins.dev.lspkind-config")
